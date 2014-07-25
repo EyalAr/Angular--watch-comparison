0afb2ed74cf4cd6af2ad8a1b98d99c6bef9b9215
Eyal Arubas <eyalarubas@gmail.com>
2014-07-25 19:52:17 +0800

    add control panel only preview

--- scripts/boxesCtrl.js
+++ scripts/boxesCtrl.js
@@ -1,6 +1,5 @@
 angular.module('boxesApp').controller('boxesCtrl', function($rootScope, $scope) {
     var n = 20 * 20, // 20 rows, 20 cols
-        speed = 30, // ms
         dataSize = 1000,
         colors = ['#ff8080', '#ff80ac', '#ff80dd', '#f280ff', '#ca80ff', '#9b80ff', '#8086ff', '#80a6ff', '#80ccff', '#80e8ff', '#80ffe3', '#80ffc6', '#80ffaa', '#95ff80', '#b9ff80', '#e3ff80', '#fff480', '#ffd080', '#ffaa80', '#ff8c80'];
 
@@ -25,6 +24,25 @@ angular.module('boxesApp').controller('boxesCtrl', function($rootScope, $scope) 
     $rootScope.time = null;
 
     $scope.$watch('colors.opened', function(color) {
+        // // Option 1:
+        // // keep the reference to the 'boxes' array, but change
+        // // references to 'box' objects inside the array.
+        // // this will not enable us to use a simple shallow-watch
+        // // on 'boxes'.
+        // for (var i = 0; i < $scope.boxes.length; i++) {
+        //     var box = $scope.boxes[i];
+        //     $scope.boxes.splice(i, 1, {
+        //         color: box.color,
+        //         opened: box.color === color,
+        //         data: box.data
+        //     });
+        // }
+
+        // Option 2:
+        // generate a completely new array, thus changing the reference
+        // in $scope.boxes.
+        // This requires more memory, but enables us to use a simple
+        // shallow-watch on 'boxes'.
         $scope.boxes = $scope.boxes.map(function(box) {
             return {
                 color: box.color,
